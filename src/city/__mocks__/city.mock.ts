86ee0147d77e8a36a7456fc73c2e49530fafbccb
guilherme <engguilhermeloli@gmail.com>
2023-07-13 20:23:36 -0300

    feat: unit tests city service

---
+++ src/city/__mocks__/city.mock.ts
@@ -0,0 +1,8 @@
+import { StateEntity } from '../entities/state.entity';
+
+export const stateMock: StateEntity = {
+  createdAt: new Date(),
+  id: 123213,
+  name: 'stateNameMock',
+  updateAt: new Date(),
+};
