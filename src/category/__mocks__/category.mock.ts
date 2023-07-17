ba69e40fb4c4e0960d721d48e5f37a217b676c11
guilherme <engguilhermeloli@gmail.com>
2023-07-17 16:17:07 -0300

    feat: find all categories function and tests using TDD

---
+++ src/category/__mocks__/category.mock.ts
@@ -0,0 +1,8 @@
+import { CategoryEntity } from '../entities/category.entity';
+
+export const categoryMock: CategoryEntity = {
+  createdAt: new Date(),
+  id: 1321,
+  name: 'categoryMock',
+  updateAt: new Date(),
+};
--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,4 +1,4 @@
-import { ProductEntity } from 'src/product/entities/product.entity';
+import { ProductEntity } from '../../product/entities/product.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'category' })
@@ -16,5 +16,5 @@ export class CategoryEntity {
   updateAt: Date;
 
   @OneToMany(() => ProductEntity, (product: ProductEntity) => product.category)
-  products: ProductEntity;
+  products?: ProductEntity;
 }
